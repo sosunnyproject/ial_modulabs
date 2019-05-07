5f70b75b03352c14e9a50e84523e535869fe905e
sosunnyproject <parksosun1103@gmail.com>
2019-05-08 07:26:33 +0900

    black and white

--- 0508video_pixel_variation/sketch.js
+++ 0508video_pixel_variation/sketch.js
@@ -1,27 +1,30 @@
-//video copy and paste
+//black and white
 var video;
 
 function setup() {
   createCanvas(320, 240);
   pixelDensity(1);
   video = createCapture(VIDEO);
-  video.size(320, 240);
+  video.size(width, height);
 }
 
 function draw() {
-  background(51);
+  background(0);
   video.loadPixels(); // pixels of video
   loadPixels(); // pixels of canvas
-  for(var y = 0; y < height; y++) {
-    for (var x = 0; x < width; x++) {
-      var index = (x + y * width) * 4;
-      var r = video.pixels[index+0];
-      var g = video.pixels[index+1];
-      var b = video.pixels[index+2];
 
-      pixels[index+0] = r;
-      pixels[index+1] = g;
-      pixels[index+2] = b;
+  for (var y = 0; y < video.height; y++) {
+    for (var x = 0; x < video.width; x++) {
+      var index = (x + y * video.width) * 4;
+      var r = video.pixels[index + 0];
+      var g = video.pixels[index + 1];
+      var b = video.pixels[index + 2];
+
+      var bright = (r + g + b) / 3;
+
+      pixels[index+0] = bright;
+      pixels[index+1] = bright;
+      pixels[index+2] = bright;
       pixels[index+3] = 255;
     }
   }
