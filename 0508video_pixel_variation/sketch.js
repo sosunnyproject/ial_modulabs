2e7921ba0b2abf7103fce3988cf30612f07a4f42
sosunnyproject <parksosun1103@gmail.com>
2019-05-08 07:27:18 +0900

    rect and scaleChange

--- 0508video_pixel_variation/sketch.js
+++ 0508video_pixel_variation/sketch.js
@@ -1,32 +1,35 @@
-//black and white
+// black and white rectangle
+// smaller videoSize
 var video;
+var vScale = 12;
 
 function setup() {
   createCanvas(320, 240);
   pixelDensity(1);
   video = createCapture(VIDEO);
-  video.size(width, height);
+  video.size(width/vScale, height/vScale)
+  // ㄴ original video size.
+  // ㄴ divide by vScale: makes less lagging.
 }
 
 function draw() {
-  background(0);
-  video.loadPixels(); // pixels of video
-  loadPixels(); // pixels of canvas
-
-  for (var y = 0; y < video.height; y++) {
+  background(51);
+  video.loadPixels();
+  loadPixels();
+  for(var y = 0; y < video.height; y++) {
     for (var x = 0; x < video.width; x++) {
       var index = (x + y * video.width) * 4;
-      var r = video.pixels[index + 0];
-      var g = video.pixels[index + 1];
-      var b = video.pixels[index + 2];
+
+      var r = video.pixels[index+0];
+      var g = video.pixels[index+1];
+      var b = video.pixels[index+2];
 
       var bright = (r + g + b) / 3;
 
-      pixels[index+0] = bright;
-      pixels[index+1] = bright;
-      pixels[index+2] = bright;
-      pixels[index+3] = 255;
+      fill(bright);
+
+      rect(x*vScale, y*vScale, vScale, vScale);
+
     }
   }
-  updatePixels();
 }
