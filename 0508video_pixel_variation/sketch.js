ca7dc93363dbd41b67cfeafd92d2e40146af0aa5
sosunnyproject <parksosun1103@gmail.com>
2019-05-08 07:32:05 +0900

    variation

--- 0508video_pixel_variation/sketch.js
+++ 0508video_pixel_variation/sketch.js
@@ -5,10 +5,7 @@ function setup() {
   createCanvas(320, 240);
   pixelDensity(1);
   video = createCapture(VIDEO);
-  // video.size(320, 240);
   video.size(width/vScale, height/vScale)
-  // ㄴ original video size.
-  // ㄴ divide by vScale: makes less lagging.
 }
 
 function draw() {
@@ -17,7 +14,6 @@ function draw() {
   loadPixels(); // pixels of canvas
   for(var y = 0; y < video.height; y++) {
     for (var x = 0; x < video.width; x++) {
-      // var index = (x + y * video.width) * 4;
       var index = ((video.width-x+1) + y * video.width)*4;
       var r = video.pixels[index+0];
       var g = video.pixels[index+1];
@@ -29,7 +25,6 @@ function draw() {
       noStroke();
       fill(map(bright, 0, 225, 100, 50), map(sin(bright),-1, 1, 200, 0), map(tan(bright),-1, 1, 200, 100) );
       rectMode(CENTER);
-      // rect(x*vScale, y*vScale,w, w);
         if ( 5 < w && w < 7 ) {
           rect(x*vScale, y*vScale, w, w);
         } else if ( w >= 7) {
@@ -37,5 +32,4 @@ function draw() {
         }
     }
   }
-  // updatePixels();
 }
